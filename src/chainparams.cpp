beb69441abfa73a1766cae3c07e0ae63f5e56e4b
IgnitionDev <info@ignitioncoin.org>
2017-12-19 02:59:17 +0100

    Starting Ignition Blockchain

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -85,38 +85,7 @@ public:
 		genesis.nBits = 520159231;
 		genesis.nNonce = 47950;
 
-		//      //  start
-				                                       // if (true && genesis.GetHash() != hashGenesisBlock)
-				                                       //                {
-				                                       //                    printf("Searching for genesis block...\n");
-				                                       //                    uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
-				                                       //                    uint256 thash;
-                                               //
-				                                       //                    while (true)
-				                                       //                    {
-				                                       //                        thash = genesis.GetHash();
-				                                       //                        if (thash <= hashTarget)
-				                                       //                            break;
-				                                       //                        if ((genesis.nNonce & 0xFFF) == 0)
-				                                       //                        {
-				                                       //                            printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
-				                                       //                        }
-				                                       //                        ++genesis.nNonce;
-				                                       //                        if (genesis.nNonce == 0)
-				                                       //                        {
-				                                       //                            printf("NONCE WRAPPED, incrementing time\n");
-				                                       //                            ++genesis.nTime;
-				                                       //                        }
-				                                       //                    }
-				                                       //                    printf("genesis.nTime = %u \n", genesis.nTime);
-				                                       //                    printf("genesis.nNonce = %u \n", genesis.nNonce);
-				                                       //                    printf("genesis.nVersion = %u \n", genesis.nVersion);
-				                                       //                    printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str()); //first this, then comment this line out and uncomment the one under.
-				                                       //                    printf("genesis.hashMerkleRoot = %s \n", genesis.hashMerkleRoot.ToString().c_str()); //improvised. worked for me, to find merkle root
-                                               //
-				                                       //                }
-				                                //
-				                                //        //end
+		
 
 		hashGenesisBlock = genesis.GetHash();
 
