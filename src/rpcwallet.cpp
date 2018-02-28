a51ad59e80be98c23e37661e7d1d79e4d3375184
Jayray <jeremy.richert1@gmail.com>
2018-02-28 23:22:55 +0100

    Limit timeout to INT32_MAX
    The timeout argument in walletpassphrase is limited to INT32_MAX to be sure that the conversion to seconds will work on every compiler.

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1963,9 +1963,9 @@ Value walletpassphrase(const Array& params, bool fHelp)
     pwalletMain->TopUpKeyPool();
 
     int64_t nSleepTime = params[1].get_int64();
-    // If the timeout value is too large, the conversion from nSleepTime to seconds results
-    // in a zero or negative value and the wallet unlocking will fail.
-    if (boost::posix_time::seconds(nSleepTime) <= boost::posix_time::seconds(0))
+    // If the timeout value is too large or negative, the conversion from nSleepTime to seconds
+    // results in a negative value and the wallet unlocking will fail.
+    if (nSleepTime > INT32_MAX || nSleepTime < 0)
     {
         pwalletMain->Lock();
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Error: The timeout value entered was incorrect.");
