9cfb45d2b8252d5a87034694e3a73fea161dcdf3
Unknown <techniumunlimited@Gmail.com>
2018-06-27 00:29:59 -0700

    Cleaned Up Unix Upgrade Script

--- scripts/upgrade-unix.sh
+++ scripts/upgrade-unix.sh
@@ -2,7 +2,6 @@
 ./install-dependencies.sh
 NB_CORES=`nproc`
 sudo killall -9 ignitiond
-mkdir ~/ignitionbackup
 rm /usr/local/bin/ignitiond
 if [ -f "/usr/local/bin/ignitiond" ]; then
   rm /usr/local/bin/ignitiond
@@ -24,6 +23,7 @@ qmake CONFIG+=debug
 make -j$NB_CORES
 cp bin/ignitiond /usr/local/bin/
 if [ -d "~/.Ignition/" ]; then
+  mkdir ~/ignitionbackup
   cd ~/.Ignition/
   rm -r smsgStore
   rm -r smsgDB
@@ -33,5 +33,6 @@ if [ -d "~/.Ignition/" ]; then
   rm -r database
   rm -r txleveldb
   cp -r * ~/ignitionbackup
+  echo "- Backed up files (including wallet.dat) are in ~/ignitionbackup -"
 fi
-echo "Upgrade Complete - You can now run ignitiond or fill out the config file in ~/Ignition - Backed up files (including wallet.dat) are in ~/ignitionbackup"
+echo "Upgrade Complete - You can now run ignitiond or fill out the config file in ~/Ignition"
