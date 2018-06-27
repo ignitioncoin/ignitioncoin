866126e4a6a37b22b2d395ea7043a8ea04c48a49
Unknown <techniumunlimited@Gmail.com>
2018-06-26 22:56:16 -0700

    Updated Build-Unix Script - Added Arguments
    Now can do ./build-unix.sh --with-gui or ./build-unix.sh --install-dependencies as args, to do the two options or build base linux bin (no args)

--- scripts/build-unix.sh
+++ scripts/build-unix.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 NB_CORES=`nproc`
 
+for var in "$@"
+do
+    if [ $var = "--install-dependencies" ]; then
+        ./install-dependencies.sh
+    fi
+done
+
 cd ../src
 make -j$NB_CORES -f makefile.unix
-cd ..
-qmake CONFIG+=debug
-make -j$NB_CORES
+
+for var in "$@"
+do
+    if [ $var = "--with-gui" ]; then
+        cd ..
+        qmake CONFIG+=debug
+        make -j$NB_CORES
+    fi
+done
