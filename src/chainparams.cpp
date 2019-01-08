d9977a7ffb0adabd62c4e93016042162a9b11bab
Jayray <jeremy.richert1@gmail.com>
2019-01-08 23:25:29 +0100

    Last PoW block set after fork 2
    Last PoW block = fork 2 + 200 blocks

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -112,7 +112,7 @@ public:
 		//strSporkKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
 		//strMasternodePaymentsPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
 		strDarksendPoolDummyAddress = "i7FBJNGDmEsU5wx2m3xw85N8kRgCqA8S7L";
-		nLastPOWBlock = 100000000;
+		nLastPOWBlock = GetForkHeightTwo() + 200;
 		nPOSStartBlock = 1;
 	}
 
@@ -169,7 +169,7 @@ public:
 
 		convertSeeds(vFixedSeeds, pnTestnetSeed, ARRAYLEN(pnTestnetSeed), nDefaultPort);
 
-		nLastPOWBlock = 0x7fffffff;
+		nLastPOWBlock = GetForkHeightTwo() + 20;
 	}
 	virtual Network NetworkID() const { return CChainParams::TESTNET; }
 };
