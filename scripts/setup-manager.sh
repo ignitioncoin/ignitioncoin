4289fffd2f0db77a334cd2780052a3b78f575e23
Unknown <techniumunlimited@Gmail.com>
2019-01-14 10:59:11 -0800

    Add Create_Config Function
    Will fill in randomly generated U/N & Password for RPC and more

--- scripts/setup-manager.sh
+++ scripts/setup-manager.sh
@@ -86,3 +86,21 @@ EOF
     exit 1
   fi
 }
+
+function create_config() {
+  mkdir $CONFIGFOLDER >/dev/null 2>&1
+  RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
+  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
+  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
+rpcuser=$RPCUSER
+rpcpassword=$RPCPASSWORD
+rpcport=$RPC_PORT
+rpcallowip=127.0.0.1
+listen=1
+server=1
+daemon=1
+maxconnections=50
+port=$COIN_PORT
+EOF
+}
+
