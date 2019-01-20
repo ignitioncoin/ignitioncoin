5e7c6fa09a80f919eac3a13d298c5bf242b34991
Unknown <techniumunlimited@Gmail.com>
2019-01-20 02:15:43 -0800

    Added back forgotten vars

--- scripts/checkswap.sh
+++ scripts/checkswap.sh
@@ -3,6 +3,15 @@
 #    grep -q "swapfile" /etc/fstab
 #    # if swap doesn't exist, create it
 #    if [ $? -ne 0 ]; then
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+BLUE_READ=$'\e[34m'
+NC='\033[0m'
+NC_READ=$'\e[0m'
+SWAP_FILE="$HOME/ignitioncoin-swap"
+MEMORY_SIZE=`grep MemTotal /proc/meminfo | awk '{print $2}'`
+
 if [ "$MEMORY_SIZE" = "" ]; then
 	echo -e "\n${GREEN}Adding 4G swap to compile${NC}"
 	sudo fallocate -l 4G $SWAP_FILE
