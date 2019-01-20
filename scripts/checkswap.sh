7584547a023e4e29dc141a151ce6ef6698491ed3
Unknown <techniumunlimited@Gmail.com>
2019-01-20 01:51:08 -0800

    Fix for when swap size is null

--- scripts/checkswap.sh
+++ scripts/checkswap.sh
@@ -3,7 +3,13 @@
 #    grep -q "swapfile" /etc/fstab
 #    # if swap doesn't exist, create it
 #    if [ $? -ne 0 ]; then
-if [ "$MEMORY_SIZE" -lt "2000000" ]; then
+if [ "$MEMORY_SIZE" = "" ]; then
+	echo -e "\n${GREEN}Adding 4G swap to compile${NC}"
+	sudo fallocate -l 4G $SWAP_FILE
+	chmod 600 /swapfile
+	sudo mkswap $SWAP_FILE
+	sudo swapon $SWAP_FILE
+elif [ "$MEMORY_SIZE" -lt "2000000" ]; then
 	echo -e "\n${GREEN}Adding 4G swap to compile${NC}"
 	sudo fallocate -l 4G $SWAP_FILE
 	chmod 600 /swapfile
