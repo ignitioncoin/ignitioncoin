c2cc4ffb9381c984d701276362b85111bf0f88e6
Technium <techniumunlimited@gmail.com>
2019-05-21 16:52:39 -0700

    fix typo in comments
    said max was 1m, when is actually 5m coins

--- src/main.h
+++ src/main.h
@@ -67,7 +67,7 @@ static const int64_t MIN_TX_FEE = 1000;
 /** Fees smaller than this (in satoshi) are considered zero fee (for relaying) */
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 /** No amount larger than this (in satoshi) is valid */
-static const int64_t MAX_MONEY = 5000000 * COIN; // 1M PoW coins
+static const int64_t MAX_MONEY = 5000000 * COIN; // 5M PoW coins
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
