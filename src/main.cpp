9c72080f5eef0d05c4dbb51266aa6a7dfa9aa2d9
Technium <techniumunlimited@gmail.com>
2019-05-23 06:22:30 -0700

    Enclose math in parenthesis

--- src/main.cpp
+++ src/main.cpp
@@ -156,7 +156,7 @@ const int GetMaxBlockSize()
     {
         return 20000000;
     }
-    ret = (trunc((pindexBest->nHeight / 525,600)) - 5) + 20 * 1000000;
+    ret = ((trunc((pindexBest->nHeight / 525,600)) - 5) + 20) * 1000000;
     if (ret < 20000000) {
         ret = 20000000;
     }
