1a89c84465ddac46a769c31805b43b013e6d67e5
Technium <techniumunlimited@gmail.com>
2019-05-26 00:03:41 -0700

    Fix inline funcs by moving to header, and fix missing ()'s

--- src/main.h
+++ src/main.h
@@ -95,12 +95,17 @@ static const int nTestnetForkTwo = 700;
 const int GetForkHeightOne();
 const int GetForkHeightTwo();
 
-inline const int GetMaxBlockSize();
-inline const int GetMaxBlockSizeGen();
-inline const int GetMaxTransactionSize();
-inline const int GetMaxBlockSigOps();
-inline const int GetMaxTransactionSigOps();
-inline const int GetMaxOrphanTransactionSize();
+const int GetMaxBlockSize();
+
+inline const int GetMaxBlockSizeGen() { return GetMaxBlockSize() / 2; }
+
+inline const int GetMaxTransactionSize() { return GetMaxBlockSizeGen() / 5; }
+
+inline const int GetMaxBlockSigOps() { return GetMaxBlockSize() / 50; }
+
+inline const int GetMaxTransactionSigOps() { return GetMaxBlockSigOps() / 5; }
+
+inline const int GetMaxOrphanTransactionSize() { return GetMaxBlockSize() / 100; }
 
 inline int64_t GetMNCollateral(int nHeight) { return 3000; }
 
